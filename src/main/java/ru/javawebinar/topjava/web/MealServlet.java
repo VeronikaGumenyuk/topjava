fa1824644c8a4998eb4ba148b75df6f303409938
Veronika Gumenyuk <jenaro1375@gmail.com>
2020-02-17 19:08:05 +0300

    Add logging

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +12,12 @@ import java.io.IOException;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
-    private static final Logger log = getLogger(MealServlet.class);
-
+   // private static final Logger log = getLogger(MealServlet.class);
+        private static final Logger LOG = getLogger(MealServlet.class);
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.debug("redirect to meals");
+        LOG.debug("redirect to meals");
 
         req.getRequestDispatcher("/meals.jsp").forward(req, resp);
 
